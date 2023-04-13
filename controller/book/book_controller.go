ba400e6a50f3af43c739633b343aaae141c4654b
Ramdhani Michandari <ramdhani.michandari@privyid.tech>
2023-04-13 14:46:58 +0700

    Revert "test restore"
    This reverts commit d6f5624a4cdb50763c1289647f398b9ffe262606.

--- controller/book/book_controller.go
+++ controller/book/book_controller.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -22,17 +21,6 @@ func NewBookController(service service.InterfaceBookService) InterfaceBookContro
 	}
 }
 
-func (b *bookController) RetrieveDataBookInChannel(resp http.ResponseWriter, req *http.Request) {
-	books, err := b.bookService.GetAllUsingChannel()
-
-	if err != nil {
-		_ = function.SendResponse(resp, 500, "something when wrong", err)
-	}
-
-	_ = function.SendResponse(resp, 200, "success", books)
-	return
-}
-
 func (b *bookController) GetBooks(resp http.ResponseWriter, req *http.Request) {
 	books, err := b.bookService.FindAll()
 
@@ -48,8 +36,7 @@ func (b *bookController) CreateBook(resp http.ResponseWriter, req *http.Request)
 	var book entity.Book
 
 	book.Title = req.FormValue("title")
-	authorID, _ := strconv.Atoi(req.FormValue("author_id"))
-	book.AuthorID = authorID
+	book.Author = req.FormValue("author")
 	book.Genre = req.FormValue("genre")
 	book.Page = req.FormValue("page")
 
@@ -60,26 +47,6 @@ func (b *bookController) CreateBook(resp http.ResponseWriter, req *http.Request)
 	}
 
 	_ = function.SendResponse(resp, 201, "success", book)
-	return
-}
-
-func (b *bookController) BulkInsert(resp http.ResponseWriter, req *http.Request) {
-	var books []entity.Book
-
-	err := json.NewDecoder(req.Body).Decode(&books)
-	if err != nil {
-		_ = function.SendResponse(resp, 500, "something when wrong", err)
-		return
-	}
-
-	respBooks, errCreate := b.bookService.BulkInsert(books)
-
-	if errCreate != nil {
-		_ = function.SendResponse(resp, 500, "something when wrong", err)
-		return
-	}
-
-	_ = function.SendResponse(resp, 201, "success", respBooks)
 	return
 }
 
@@ -146,35 +113,34 @@ func (b *bookController) DeleteBook(resp http.ResponseWriter, req *http.Request)
 	return
 }
 
-//func (b *bookController) UpdateBook(resp http.ResponseWriter, req *http.Request) {
-//	params := mux.Vars(req)
-//
-//	id, errConv := strconv.ParseInt(params["id"], 10, 64)
-//
-//	if errConv != nil {
-//		log.Println("terjadi kesalahan ketika mengkonversi id ke integer", errConv)
-//	}
-//
-//	book, err := b.bookService.Detail(int(id))
-//
-//	if err != nil {
-//		_ = function.SendResponse(resp, 404, fmt.Sprintf("tidak ada data dengan id %d", id), err)
-//		return
-//	}
-//
-//	book.Title = req.FormValue("title")
-//	authorID, _ := strconv.Atoi(req.FormValue("author_id"))
-//	book.AuthorID = authorID
-//	book.Genre = req.FormValue("genre")
-//	book.Page = req.FormValue("page")
-//	book.ID = uint(id)
-//
-//	err = b.bookService.Update(int(id), book)
-//	if err != nil {
-//		_ = function.SendResponse(resp, 500, "something when wrong", err)
-//		return
-//	}
-//
-//	_ = function.SendResponse(resp, 200, "success", fmt.Sprintf("sukses mengupdate data dengan id %d", id))
-//	return
-//}
+func (b *bookController) UpdateBook(resp http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+
+	id, errConv := strconv.ParseInt(params["id"], 10, 64)
+
+	if errConv != nil {
+		log.Println("terjadi kesalahan ketika mengkonversi id ke integer", errConv)
+	}
+
+	book, err := b.bookService.Detail(int(id))
+
+	if err != nil {
+		_ = function.SendResponse(resp, 404, fmt.Sprintf("tidak ada data dengan id %d", id), err)
+		return
+	}
+
+	book.Title = req.FormValue("title")
+	book.Author = req.FormValue("author")
+	book.Genre = req.FormValue("genre")
+	book.Page = req.FormValue("page")
+	book.ID = uint(id)
+
+	err = b.bookService.Update(int(id), book)
+	if err != nil {
+		_ = function.SendResponse(resp, 500, "something when wrong", err)
+		return
+	}
+
+	_ = function.SendResponse(resp, 200, "success", fmt.Sprintf("sukses mengupdate data dengan id %d", id))
+	return
+}
